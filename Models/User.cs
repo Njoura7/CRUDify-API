1f1becb129e83562fe9828ff61ab6127fff49078
Njoura7 <njourawebdev@gmail.com>
2025-01-04 18:55:30 +0100

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace CRUDify_API.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string? Color { get; set; }
+    }
+}
